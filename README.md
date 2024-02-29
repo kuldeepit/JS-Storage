ba030a3a0831451f5510e155b84d68631015fe40
kuldeepit <72602801+kuldeepit@users.noreply.github.com>
2024-03-01 00:12:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS-Storage
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/kuldeepit/JS-Storage)
---
+++ script.js
@@ -0,0 +1,31 @@
+//Challenge 1: Store the user's first name, last name, age, country, and state in your browser's localStorage using the setItem() method and print the localStorage in the console.
+localStorage.setItem('firstName', 'Kuldeep');
+localStorage.setItem('lastName', 'Raghav');
+localStorage.setItem('age', '24');
+localStorage.setItem('country', 'India');
+localStorage.setItem('state', 'Delhi');
+console.log(localStorage);
+
+//Challenge 2: Get the users data stored on your browser's localStorage using the getItem() method and print the localStorage in the console.
+console.log(localStorage.getItem('firstName'));
+console.log(localStorage.getItem('lastName'));
+console.log(localStorage.getItem('age'));
+console.log(localStorage.getItem('country'));
+console.log(localStorage.getItem('state'));
+
+//Challenge 3: Using removeItem() method, remove the "state" data stored in your browser's localStorage and print the localStorage in the console.
+localStorage.removeItem('state');
+console.log(localStorage);
+
+//Challenge 4: Using clear() method to remove all the data stored in your browser's localStorage and print the localStorage in the console.
+localStorage.clear();
+console.log(localStorage);
+
+//Challenge 5: Using JSON.stringify() convert the given object into the string and print the same.
+const user = {
+  firstName: 'Rajat',
+  age: 25,
+  skills: ['HTML', 'CSS', 'JS', 'React'],
+};
+const userString = JSON.stringify(user);
+console.log(userString);
